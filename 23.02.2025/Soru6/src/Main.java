dad089688f23585a60bb7715faf136325bfb56dc
Edefiye Erkek <edefiyeerkek@Edefiye-MacBook-Air.local>
2025-03-02 00:27:20 +0300

    Soru6 Odev

---
+++ 23.02.2025/Soru6/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //SORU5
+        Scanner scanner2 = new Scanner(System.in);
+        ArrayList<String> kelimeler = new ArrayList<>();
+
+        System.out.println("10 Tane Kelime Giriniz...");
+        for (int i = 0; i < 10; i++) {
+            System.out.print((i + 1) + ". Kelimeyi girin: ");
+            String kelime = scanner2.nextLine();
+            kelimeler.add(kelime);
+        }
+        Collections.sort(kelimeler);
+        System.out.println("\nAlfabetik sıraya göre kelimeler:");
+        for (String kelime : kelimeler) {
+            System.out.println(kelime);
+        }
+    }
+}
