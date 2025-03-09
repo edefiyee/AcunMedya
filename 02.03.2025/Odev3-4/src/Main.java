9379f5484854addda92f2339cb2f79cafeefe7cb
Edefiye <eedefiye@gmail.com>
2025-03-09 18:16:07 +0300

    Odev3-4 birlikte

---
+++ 02.03.2025/Odev3-4/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*SORU3
+        abstract clsss soyut class demektir.abstract denen yapı özünde normal sınıf memberlarıyla
+         birlikte bu abstract classı uygulayan sınıflara zoraki olarak uygulattıracak member
+         imzaları da barındıran bir yapıdır.
+         Interface, bir maske şablonu oluşturmamızı sağlayan bir yapıdır. Sağladığı şablon sayesinde
+         gereksiz ayrıntılardan bağımsız bir şekilde süreci daha düzenli hale getirir.
+
+         SORU4
+         abstract classlardan new ya da başka bir şekilde nesne üretemezsin ama bu h,çb,r zaman üretilmeyeceği
+         anlamına gelmz.Başka bir sınıfa miras verdiğinde o sınıfta nesne oluşturabilir.Abstract classlardan nesneyi
+         biz üretemeyiz ama abstract classın nesnesi olur interfacden farkı budur.
+         interfacede nesne oluşturamayız.
+         Abstract class sadece bir sınıfı miras alabilir ancak inheritance birden fazla sınıfı miras alabilir.
+       */
+
+
+
+
+
+
+    }
+}
